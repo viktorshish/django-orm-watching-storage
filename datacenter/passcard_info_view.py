77fe090691355086153bacab4bcc47bdfee85489
Viktor Shishkalov <viktorshish@gmail.com>
2023-08-29 14:26:43 +0300

    Получил все визиты по пропуску и внес обработку 404

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,27 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
+from datacenter.models import get_duration, format_duration, is_visit_long
 from django.shortcuts import render
-
+import django
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
+    this_passcard_visits = []
+    for visit in visits:
+        entered = django.utils.timezone.localtime(visit.entered_at)
+        time_left_inside = get_duration(visit)
+        duration = format_duration(time_left_inside)
+        is_strange = is_visit_long(visit, minutes=60)
+        passcard_visits = {
+            'entered_at': entered,
+            'duration': duration,
+            'is_strange': is_strange
+        }
+        this_passcard_visits.append(passcard_visits)
+        
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
