371c29926e993dea4df5de9df4412383abfe1daa
Viktor Shishkalov <viktorshish@gmail.com>
2023-08-30 21:45:10 +0300

    Исправил списки

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -9,18 +9,15 @@ from datacenter.models import Visit
 def passcard_info_view(request, passcode):
     access_card = get_object_or_404(Passcard, passcode=passcode)
     visits_passcard = Visit.objects.filter(passcard=access_card)
-
+    this_passcard_visits_serialized = []
     for visit in visits_passcard:
-        this_passcard_visits = [
-            {
+        this_passcard_visits_serialized.append({
                 'entered_at': timezone.localtime(visit.entered_at),
                 'duration': format_duration(get_duration(visit)),
                 'is_strange': is_visit_long(visit, minutes=60)
-            }
-        ]
-
+            })
     context = {
         'passcard': access_card,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': this_passcard_visits_serialized
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,19 +6,14 @@ from datacenter.models import Visit
 
 
 def storage_information_view(request):
-    remainder = Visit.objects.filter(leaved_at__isnull=True)
-
-    for visit in remainder:
-        non_closed_visits = [
-            {
-                'who_entered': visit.passcard.owner_name,
-                'entered_at': timezone.localtime(visit.entered_at),
-                'duration': format_duration(get_duration(visit))
-            }
-        ]
-
-    context = {
-        'non_closed_visits': non_closed_visits,
-    }
+    visitors_remaining = Visit.objects.filter(leaved_at__isnull=True)
+    non_closed_visits_serialized = []
+    for visit in visitors_remaining:
+        non_closed_visits_serialized.append({
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': timezone.localtime(visit.entered_at),
+            'duration': format_duration(get_duration(visit))
+        })
+    context = {'non_closed_visits': non_closed_visits_serialized}
 
     return render(request, 'storage_information.html', context)
