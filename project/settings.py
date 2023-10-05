06932e0be0bc47812fe00d0ffd8f15fb960b2c8a
Viktor Shishkalov <viktorshish@gmail.com>
2023-10-06 00:22:48 +0300

    Вынес настройку DEBUG

--- project/settings.py
+++ project/settings.py
@@ -7,11 +7,11 @@ env.read_env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env('HOST'),
-        'PORT': env('PORT'),
-        'NAME': env('NAME'),
-        'USER': env('USER'),
-        'PASSWORD': env('PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
     }
 }
 
