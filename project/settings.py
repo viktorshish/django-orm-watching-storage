8acc988aa1fc3ee3375654196016e4142ca71ccb
Viktor Shishkalov <viktorshish@gmail.com>
2023-10-07 17:46:39 +0300

    Вынес настройку ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
